dafe3db714946a3c7be6c0d43ae4687d8e7b07e2
Jefry Chiedi <jefrychiedi.dev@gmail.com>
2026-06-19 16:39:06 +0700

    fix: Adjust z-index of UserDropdown for mobile visibility

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -107,7 +107,7 @@ export default function UserDropdown() {
       <Dropdown
         isOpen={isOpen}
         onClose={closeDropdown}
-        className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
+        className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg z-[999999] dark:border-gray-800 dark:bg-gray-dark"
       >
         <div className="flex items-center justify-between gap-3 rounded-xl bg-gray-50 px-3 py-2 dark:bg-white/[0.03]">
           <div className="min-w-0 flex-1 text-right">
